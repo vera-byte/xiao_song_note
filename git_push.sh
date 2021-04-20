0c1dd94c7ce53a52b3defa313ea9bf4feaf5c3c1
mac <maci@mac.local>
2021-04-20 17:31:58 +0800

    Updated 2021-04-20 17:31:58

--- git_push.sh
+++ git_push.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env sh
-
-
-
-
 echo "😀-------正在打包项目···-------"
+
 chmod vuepress build docs
+
 echo "👏-------添加文件到仓库-------"
+
 git add .
+
 echo "😯-------写入提交信息-------"
+
 git commit -m "Updated $(date +'%Y-%m-%d %H:%M:%S')"
+
 echo "😸提交仓库"
+
 git push -u origin master
+
 echo "🤤$(date +'%Y-%m-%d %H:%M:%S')|开始到生产环境部署"
+
 res=$(curl http://bt.metoyun.com:8888/hook?access_key=qUiYRYHR26YvmMZz7jxvDiDLfaOxmVKmHID4XticOUcEg0yk&param=aaa)
-echo ‘---------------------’
+
+echo "😯-------发送WebHook-------"
+
 echo $res
+
 if [ "$res" == '{"code": 1}' ];then
+
 echo "😏 $(date +'%Y-%m-%d %H:%M:%S')|生产环境部署请求已接受."
+
 else
+
 echo "😨 $(date +'%Y-%m-%d %H:%M:%S')|部署请求未收到正常回应,请检查配置!"
+
 fi
 
+cd -
