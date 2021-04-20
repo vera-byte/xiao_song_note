ea6a52a69c5975eb3de7dd7c6eedca20d7545c3c
mac <maci@mac.local>
2021-04-20 18:15:18 +0800

    Updated 2021-04-20 18:15:18

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
             'link', // 设置 favicon.ico，注意图片放在 public 文件夹下
             {
                 rel: 'icon',
-                href: 'note/docs/.vuepress/dist/assets/img/logo.gif'
+                href: '/assets/img/logo.gif'
             }
         ],
     ],
