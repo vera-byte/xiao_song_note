167605b574813f735393bfe55958701f46f94f73
mac <maci@mac.local>
2021-05-07 16:10:46 +0800

    Updated 2021-05-07 16:10:46

--- docs/.vuepress/dist/service-worker.js
+++ docs/.vuepress/dist/service-worker.js
@@ -27,11 +27,11 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "0efa1c98f7f55af7cba30663277eaf1a"
+    "revision": "e47a655ef1003371fffbcb9fab0cc6cb"
   },
   {
     "url": "About.html",
-    "revision": "4fc7837e49fa806f7a41ca4bca68f5ff"
+    "revision": "3e887ce31695fbadade2e7583065777a"
   },
   {
     "url": "assets/css/0.styles.0ea9ec4a.css",
@@ -46,12 +46,12 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.986f205c.js",
-    "revision": "87170068bb9c1a40e1852240d7d72234"
+    "url": "assets/js/10.7abc4dfd.js",
+    "revision": "4c80d18ec05b536757e2aef44c6e88d2"
   },
   {
-    "url": "assets/js/11.ac0b9adb.js",
-    "revision": "f05685b1d80b3f27632018a98b762522"
+    "url": "assets/js/11.1f47a2f5.js",
+    "revision": "9e74448fc2b093fa27e48c95b1c920c1"
   },
   {
     "url": "assets/js/12.2d8a3b61.js",
@@ -66,56 +66,56 @@ self.__precacheManifest = [
     "revision": "4ffe2809f09d545ae6d94d836fa42b79"
   },
   {
-    "url": "assets/js/15.e0d41ce5.js",
-    "revision": "2c0d48c27bd33ce87c98be7de2610fc9"
+    "url": "assets/js/15.0180e547.js",
+    "revision": "da3e50b35ddcb209acf041f443e4634f"
   },
   {
     "url": "assets/js/16.3e0bfa1e.js",
     "revision": "bd311002f450a7b34d25055eda3248e8"
   },
   {
-    "url": "assets/js/17.e6269558.js",
-    "revision": "ba93d1af8b11f0a42385e6a5d48f7172"
+    "url": "assets/js/17.3cc17eef.js",
+    "revision": "df66f2f26124037bf046a17a0506f838"
   },
   {
-    "url": "assets/js/18.32eb0e3d.js",
-    "revision": "d6d9de7894fdd193dab5c1639dfd4370"
+    "url": "assets/js/18.e7a1aa58.js",
+    "revision": "79d441e47c0bc5e422abaa27426f045a"
   },
   {
-    "url": "assets/js/19.344ca220.js",
-    "revision": "f28e9260e194f4e548e7982f003090a8"
+    "url": "assets/js/19.25f515a9.js",
+    "revision": "f43bbd4fbdb2cd1d54b77193877f1a90"
   },
   {
     "url": "assets/js/2.9214251a.js",
     "revision": "325a931123ccd3681445173a46ed83de"
   },
   {
-    "url": "assets/js/20.63f77707.js",
-    "revision": "da6d3d51472dfd49dcca95c580f349c5"
+    "url": "assets/js/20.f638739c.js",
+    "revision": "c1a2e2b8bf6cd0c5fd0a4fe8c82b890b"
   },
   {
-    "url": "assets/js/21.1aa57827.js",
-    "revision": "15f97555aba27c5e4ccd4a5a106dcc6c"
+    "url": "assets/js/21.af6168d8.js",
+    "revision": "0d00b5402f88ada7568d4528427482d7"
   },
   {
-    "url": "assets/js/22.1068e1a3.js",
-    "revision": "41a2eb938fb38cfc32e737981dcc8d3f"
+    "url": "assets/js/22.5380ebd7.js",
+    "revision": "a0694df5400c29b0ef427c033969492e"
   },
   {
-    "url": "assets/js/23.09e65aaf.js",
-    "revision": "ef0961c675cc5e55e842134ed5b2263d"
+    "url": "assets/js/23.abc84364.js",
+    "revision": "0f9f6a062aef03257c354a2c21dfa956"
   },
   {
-    "url": "assets/js/24.a84dd9e2.js",
-    "revision": "45e2c5d80bdbbbbfd064992d8a7412f7"
+    "url": "assets/js/24.179a2fb9.js",
+    "revision": "21423ee71915a4b348c8dec3e91f55d0"
   },
   {
-    "url": "assets/js/25.f956a518.js",
-    "revision": "a18d69a4a1f9643fdc81ced7b04d552c"
+    "url": "assets/js/25.29935595.js",
+    "revision": "3450b5db48b23c05c1435b26c8b6d32a"
   },
   {
-    "url": "assets/js/26.15ffcce4.js",
-    "revision": "df32063e3bf0b84b3bafad489447f9f7"
+    "url": "assets/js/26.e85e7912.js",
+    "revision": "04fd32acfd39391b40dffbf35f3c476b"
   },
   {
     "url": "assets/js/27.d79ab1bd.js",
@@ -146,24 +146,24 @@ self.__precacheManifest = [
     "revision": "638bb3d8bf3ef327a7b7b65e1b3324c9"
   },
   {
-    "url": "assets/js/9.892cfdd5.js",
-    "revision": "5efb292ac5581ce5c5df189e7616c9df"
+    "url": "assets/js/9.569dfb7f.js",
+    "revision": "9f5418f3d5380502c61c6c5c1cc1930c"
   },
   {
-    "url": "assets/js/app.1d91e436.js",
-    "revision": "e82e770a496cb8c2eec9d3ab182b460f"
+    "url": "assets/js/app.7df8a092.js",
+    "revision": "d053f900fac6c04123bf9df3e12d7a79"
   },
   {
     "url": "category/index.html",
-    "revision": "5a8e85992f1a26f3cad4d94216f30f56"
+    "revision": "0598efc7997f8b269de926da97d798ee"
   },
   {
     "url": "egg/index.html",
-    "revision": "7d67fe67b4bc2923769d94ca520daf28"
+    "revision": "1d14042865a1aabbb1d222d037cbf9bb"
   },
   {
     "url": "electron/index.html",
-    "revision": "dc426c66e42a1701f14756b5b75c493d"
+    "revision": "d8278cd7ce80a2475055a492fd6e2fbf"
   },
   {
     "url": "html/vscode/mac.html",
@@ -175,79 +175,79 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "e5bbfe6956d7f710e731c5fc895ca361"
+    "revision": "a206ad59955e28bf42b21aefabc189df"
   },
   {
     "url": "notes/git-webhook.html",
-    "revision": "4b7ca3b03c832f75fd234a421bb03876"
+    "revision": "34ff8d1b60a5236fc79f21597046626c"
   },
   {
     "url": "notes/git.html",
-    "revision": "88c124ea8b418fb2c80cf8e7143e0589"
+    "revision": "d11f1a5cfe400f15d662b5da5f03886a"
   },
   {
     "url": "notes/index.html",
-    "revision": "22b42cf106923ca909098213f348783f"
+    "revision": "e900eb1432a633873dc1668c4345ab29"
   },
   {
     "url": "notes/nginx.html",
-    "revision": "889126e5112f3aa030516351c19d5676"
+    "revision": "14f335d13a7f5c78c219d1ffb65a2c93"
   },
   {
     "url": "notes/node.html",
-    "revision": "6833edb38b27c51258542b2e9547a854"
+    "revision": "87f703c144c0fe8748fcbde142fac020"
   },
   {
     "url": "notes/npm.html",
-    "revision": "ebbd8b3e8934a3549e92dbc7db9eb771"
+    "revision": "d742d36ee9f935a192a0bea618a853dd"
   },
   {
     "url": "notes/typeorm.html",
-    "revision": "3aae13df0eb9e6e7ea226f29ebc61a8a"
+    "revision": "b92ba4a79272af5207f9ddeb98b54fe4"
   },
   {
     "url": "notes/vscode.html",
-    "revision": "347fbfcbf5829d2015b3c0165d249a4e"
+    "revision": "0024a317bd9876c5fb93763353e573f7"
   },
   {
     "url": "tag/index.html",
-    "revision": "389feb9c48ffce600ca3dc0cd782478b"
+    "revision": "80a68c1e0317deffa579ee9671978ef6"
   },
   {
     "url": "uniapp/使用Vscode开发Uniapp项目.html",
-    "revision": "591daecc45fbb7aa38d4e7d70256ce02"
+    "revision": "7e97486953f21e4b5ff41a469f3b073b"
   },
   {
     "url": "uniapp/index.html",
-    "revision": "7af931538ff655a04e10f3ca4e6edc66"
+    "revision": "1483f47e2307696ac0b6d783399e3930"
   },
   {
     "url": "uniapp/uniapp.html",
-    "revision": "aafac5ab3557c34670bfe57ed64b72f1"
+    "revision": "b142b663e048041d3b07e541acb79a95"
   },
   {
     "url": "uniapp/Uniapp引入自定义字体.html",
-    "revision": "534dba75819fbfb524934c9092805e07"
+    "revision": "65cade0b554a816a2b87d43ccd8ec2e1"
   },
   {
     "url": "website/index.html",
-    "revision": "13bb8eb59627c5c0c712ff79741cd715"
+    "revision": "9a91d5dad1b2ebdc3c6dfe14bf07ba3a"
   },
   {
     "url": "website/package.html",
-    "revision": "4f7e721a0356d98bbc61a772ffe979e0"
+    "revision": "f986aed63c58fc8a6902abb94a3c687c"
   },
   {
     "url": "website/rearend.html",
-    "revision": "fcf0c7b9399cc4bc4faf2f24817eba23"
+    "revision": "04e386c1adb4ed71c25fe7a876e0ce68"
   },
   {
     "url": "website/tools.html",
-    "revision": "a2dc93075b20a884ffbdffa866b7cb5d"
+    "revision": "1434a27caad42f6e29673fbebf909eb4"
   },
   {
     "url": "website/webclient.html",
-    "revision": "156b676b09a6d9da8f145c052dd7687a"
+    "revision": "7ee86bee4bf77e0a56fe7f2bf2dc0446"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
--- docs/uniapp/Uniapp引入自定义字体.md
+++ docs/uniapp/Uniapp引入自定义字体.md
@@ -6,24 +6,55 @@
 3.  解压字体套件压缩包
 4.  将字体文件导入到项目下的```static```文件目录下
 5.  将压缩包里的css文件复制到你的项目，代码如下
-``` css
-//将路径改成你的字体文件目录
-@font-face {
-  font-family: 'DIN Alternate';
-  src: url('~@/static/fonts/DINAlternate-Bold.woff2') format('woff2'),
-    url('~@/static/fonts/DINAlternate-Bold.woff') format('woff');
-  font-weight: bold;
-  font-style: normal;
-  font-display: swap;
-}
+<CodeSwitcher :languages="{file:'font file',base64:'Font to Base64 encode'}">
 
-//设置一个class名使用此字体
-.DIN_Alternate_Bold {
-  font-family: 'DIN Alternate' !important;
-  font-style: normal;
-}
+<template v-slot:file>
+
+    ``` css
+  //将路径改成你的字体文件目录
+  @font-face {
+    font-family: 'DIN Alternate';
+    src: url('~@/static/fonts/DINAlternate-Bold.woff2') format('woff2'),
+      url('~@/static/fonts/DINAlternate-Bold.woff') format('woff');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
+  }
+  
+
+  //设置一个class名使用此字体
+  .DIN_Alternate_Bold {
+    font-family: 'DIN Alternate' !important;
+    font-style: normal;
+  }
 
 ```
+
+</template>
+<template v-slot:base64>
+
+``` css
+
+//需要在transfonter转换时勾选Base64 encode
+  @font-face {
+    font-family: 'DIN Alternate';
+    src: url('data:font/woff2;charset=utf-8;base64,·····') format('woff2'),
+      url('data:font/woff2;charset=utf-8;base64,····') format('woff');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  //设置一个class名使用此字体
+  .DIN_Alternate_Bold {
+    font-family: 'DIN Alternate' !important;
+    font-style: normal;
+  }
+
+```
+
+</template>
+</CodeSwitcher>
 
 6. 全局引入，在App.vue 引入css
 ``` css 
