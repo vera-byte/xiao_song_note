1609bc54fc2797fe1e1e8d90496bbaf59e8b663c
mac <mac@macdeiMac.local>
2021-06-11 20:40:17 +0800

    Updated 2021-06-11 20:40:17

--- docs/notes/sidebar_contents.js
+++ docs/notes/sidebar_contents.js
@@ -1,13 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-05-21 15:23:56
- * @LastEditTime: 2021-06-11 11:34:58
+ * @LastEditTime: 2021-06-11 16:35:37
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /note/docs/notes/sidebar_contents.js
  */
 module.exports = [
-
 	"git-webhook",
 	"git",
 	"nginx",
@@ -16,8 +15,4 @@ module.exports = [
 	"typeorm",
 	"vscode",
 	"brew"
-
-
-
-
 ];
