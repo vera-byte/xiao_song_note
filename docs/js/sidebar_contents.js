0be588c9d73ba4fa212269076d0697e7954da54b
mac <mac@macdeiMac.local>
2021-07-01 11:34:39 +0800

    Updated 2021-07-01 11:34:39

--- docs/js/sidebar_contents.js
+++ docs/js/sidebar_contents.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-05-21 15:23:56
- * @LastEditTime: 2021-06-22 23:46:02
+ * @LastEditTime: 2021-07-01 11:34:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /note/docs/js/sidebar_contents.js
@@ -9,5 +9,6 @@
 module.exports = [
 	"纯前端实现模糊搜索",
 	"折扣计算",
-	"PC页面适配"
+	"PC页面适配",
+	"promise"
 ];
